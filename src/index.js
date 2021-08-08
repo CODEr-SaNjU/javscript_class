c4c5af628e4c0ecae29af1b9dd6f853ba0713226
Brajmohan <brajmohan.kansal@arthonsys.co>
2021-08-08 07:43:17 +0530

    miniom javascript

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ btnclick = document.querySelector('#btn-click')
 userinput = document.querySelector('#text-input')
 output = document.querySelector('.output-area')
 btnclick.style.backgroundColor = "green"
-function showmessage(){
-    output.innerText = "this is transale "+ " "+ userinput.value
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function getTransale(text){
+    return url + "?" + "text=" + text
+}
+
+function errorcontroler(error){
+    // console.log("error catch here",error)
+    alert("somtihin wrong ",error)
+}
+
+function showMessage(){
+    var userinputvalue = userinput.value
+    fetch(getTransale(userinputvalue))
+    .then(response => response.json())
+    .then(json =>{
+        var transaltedcontent = json.contents.translated;
+        output.innerText = transaltedcontent
+    })
+    .catch(errorcontroler)
 };
 
 btnclick.addEventListener('click'
-    ,showmessage)
+    ,showMessage)
 
